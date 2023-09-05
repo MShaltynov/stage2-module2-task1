ece373342dd5fe7c740c44a9f71420451ba834c6
Mikhail <shaltynovm@gmail.com>
2023-09-05 18:59:19 +0530

    added @WebServlet annotations

--- src/main/java/com/example/servlet/AddUserServlet.java
+++ src/main/java/com/example/servlet/AddUserServlet.java
@@ -5,11 +5,12 @@ import com.example.Warehouse;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-
+@WebServlet
 public class AddUserServlet extends HttpServlet {
     //write your code here!
 
